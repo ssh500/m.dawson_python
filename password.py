284d3ed45f11331f199afc8ae2c0274d6c8e613c
ssh500 <s.sapsan49@gmail.com>
2024-07-23 21:10:38 +0300

    Add file password.py

---
+++ password.py
@@ -0,0 +1,7 @@
+# Password
+# Демонстрирует использование конструкции if
+print('Добро пожаловать к нам в "System Security Inc."')
+print('Безопасность наше второе имя\n')
+password = input("Введите пароль: ")
+if password == "secret":
+    print("Доступ открыт.")
